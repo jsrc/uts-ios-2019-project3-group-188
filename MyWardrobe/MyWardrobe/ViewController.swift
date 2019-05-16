b27cc13f2def00a70cdb6e97ea463c91009282e9
jsrc <onriver@outlook.com>
2019-05-17 00:11:47 +1000

    init

---
+++ MyWardrobe/MyWardrobe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyWardrobe
+//
+//  Created by 江上 on 2019/5/17.
+//  Copyright © 2019 UTS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
