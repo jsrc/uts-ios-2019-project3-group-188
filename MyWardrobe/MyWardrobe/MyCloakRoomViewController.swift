bf4200a3652613450e6964f303d0459a7ead5c14
jsrc <onriver@outlook.com>
2019-05-27 21:43:36 +1000

    MyCloakRoom func

---
+++ MyWardrobe/MyWardrobe/MyCloakRoomViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MyCloakRoomViewController.swift
+//  MyWardrobe
+//
+//  Created by 江上 on 2019/5/27.
+//  Copyright © 2019 UTS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyCloakRoomViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBOutlet weak var upperWearImgView: UIImageView!
+    @IBOutlet weak var pantsImgView: UIImageView!
+    @IBOutlet weak var shooesImgView: UIImageView!
+    
+    @IBAction func startMatching() {
+        
+    }
+    
+}
