a94ebbd7299f68b71e8799893cb5e27e7bf25858
jsrc <onriver@outlook.com>
2019-06-05 23:10:36 +1000

    bug fixed

--- MyWardrobe/MyWardrobe/MyCloakRoomViewController.swift
+++ MyWardrobe/MyWardrobe/MyCloakRoomViewController.swift
@@ -23,39 +23,34 @@ class MyCloakRoomViewController: UIViewController {
     @IBAction func startMatching() {
         
         let fileManager = FileManager.default
-        let urlForDocument = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
-        let url = urlForDocument[0] as URL
+        let urlForDocument = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0] as URL
         
-//        var hatsUrl: URL = url.appendingPathComponent("Hats", isDirectory: true)
-        let upperWearUrl: URL = url.appendingPathComponent("UpperWear", isDirectory: true)
-        let pantsUrl: URL = url.appendingPathComponent("Pants", isDirectory: true)
-        let shooseUrl: URL = url.appendingPathComponent("Shooes", isDirectory: true)
-        
-//        let contentsOfURL = try? fileManager.contentsOfDirectory(at: hatsUrl, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
-        let contentsOfURL2 = try? fileManager.contentsOfDirectory(at: upperWearUrl, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
-        let contentsOfURL3 = try? fileManager.contentsOfDirectory(at: pantsUrl, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
-        let contentsOfURL4 = try? fileManager.contentsOfDirectory(at: shooseUrl, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+        let hatsUrl = try? fileManager.contentsOfDirectory(at: urlForDocument.appendingPathComponent("Hats", isDirectory: true), includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+        let upperWearUrl = try? fileManager.contentsOfDirectory(at: urlForDocument.appendingPathComponent("UpperWear", isDirectory: true), includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+        let pantsUrl = try? fileManager.contentsOfDirectory(at: urlForDocument.appendingPathComponent("Pants", isDirectory: true), includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+        let shooseUrl = try? fileManager.contentsOfDirectory(at: urlForDocument.appendingPathComponent("Shooes", isDirectory: true), includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
 
-//        let count = contentsOfURL?.count
-//        if count! > 0 {
-//            let fileData = fileManager.contents(atPath: contentsOfURL![0].path)
-//            let image = UIImage(data: fileData!)
-//        }
-        let count2 = contentsOfURL2?.count
-        if count2! > 0 {
-            let fileData = fileManager.contents(atPath: contentsOfURL2![0].path)
+        if (hatsUrl != nil && hatsUrl!.count > 0) {
+            let randomInt = Int(arc4random_uniform(UInt32(hatsUrl!.count)))
+            let fileData = fileManager.contents(atPath: hatsUrl![randomInt].path)
             let image = UIImage(data: fileData!)
             upperWearImgView.image = image
         }
-        let count3 = contentsOfURL3?.count
-        if count3! > 0 {
-            let fileData = fileManager.contents(atPath: contentsOfURL3![0].path)
+        if (upperWearUrl != nil && upperWearUrl!.count > 0) {
+            let randomInt = Int(arc4random_uniform(UInt32(upperWearUrl!.count)))
+            let fileData = fileManager.contents(atPath: upperWearUrl![randomInt].path)
+            let image = UIImage(data: fileData!)
+            upperWearImgView.image = image
+        }
+        if (pantsUrl != nil && pantsUrl!.count > 0) {
+            let randomInt = Int(arc4random_uniform(UInt32(pantsUrl!.count)))
+            let fileData = fileManager.contents(atPath: pantsUrl![randomInt].path)
             let image = UIImage(data: fileData!)
             pantsImgView.image = image
         }
-        let count4 = contentsOfURL4?.count
-        if count4! > 0 {
-            let fileData = fileManager.contents(atPath: contentsOfURL4![0].path)
+        if (shooseUrl != nil && shooseUrl!.count > 0) {
+            let randomInt = Int(arc4random_uniform(UInt32(shooseUrl!.count)))
+            let fileData = fileManager.contents(atPath: shooseUrl![randomInt].path)
             let image = UIImage(data: fileData!)
             shooesImgView.image = image
         }
